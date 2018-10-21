8dd178107291fde0298e7c69ed5b1e3450f51275
Gerald LaMountain <lamountain.g@husky.neu.edu>
2018-10-21 12:29:14 -0400

    Updates to NavJson.py

--- NavJson.py
+++ NavJson.py
@@ -1,29 +1,58 @@
-import numpy as np
-import pylab as plt
+from __future__ import print_function
+
 import ephem
 import datetime
 import json
 
-# def_sat = 
-def_loc = ('42.37', '-71.03', '0')
+class SatTrack():
 
-def load_all_satellites():
-    with open('NAV_SAT.json') as f:
-        data = json.load(f)
-    return data
+    def __init__(self):
+        # Load all SVs from JSON
+        self.data = self.load_all_satellites()
 
-def print_location_lle(data):
+        # Set the first GPS satellite as the current SV
+        l1 = self.data['GPS'][1]['id'].encode('utf-8')
+        l2 = self.data['GPS'][1]['l2'].encode('utf-8')
+        l3 = self.data['GPS'][1]['l3'].encode('utf-8')
 
-    ctime = datetime.datetime.now()
-    for sys in data.items():
-        for sat in sys[1]:
-            l1 = sat['id'].encode('utf-8')
-            l2 = sat['l2'].encode('utf-8')
-            l3 = sat['l3'].encode('utf-8')
-            sv = ephem.readtle(l1,l2,l3)
-            sv.compute(ctime)
-            print sv.sublat, sv.sublong, sv.elevation
+        # Update ephemeris for current SV
+        ctime = datetime.datetime.now()
+        self.csat = ephem.readtle(l1,l2,l3) 
+        self.csat.compute(ctime)
 
-data = load_all_satellites()
-print_location_lle(data)
+    def load_all_satellites(self):
+        with open('NAV_SAT.json') as f:
+            data = json.load(f)
+        return data
 
+    def print_location_all(self):
+
+        ctime = datetime.datetime.now()
+        for sys in self.data.items():
+            for sat in sys[1]:
+                l1 = sat['id'].encode('utf-8')
+                l2 = sat['l2'].encode('utf-8')
+                l3 = sat['l3'].encode('utf-8')
+                sv = ephem.readtle(l1,l2,l3)
+                sv.compute(ctime)
+
+                print(l1)
+                print(sv.sublat)
+                print(sv.sublong)
+                print(sv.elevation)
+                print("\n")
+                
+def lambda_handler(event, context):
+    #print("Received event: " + json.dumps(event, indent=2))
+    print("value1 = " + event['key1'])
+    print("value2 = " + event['key2'])
+    print("value3 = " + event['key3'])
+    
+    tracker.load_all_satellites()
+    tracker.print_location_all()
+    
+    return event['key1']  # Echo back the first key value
+    #raise Exception('Something went wrong')
+
+tracker = SatTrack()
+tracker.print_location_all()
