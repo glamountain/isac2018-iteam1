52640bb44cc4618080af3c475568db466c43b9ef
Gerald LaMountain <lamountain.g@husky.neu.edu>
2018-10-20 23:46:21 -0400

    Update NavJson.py to include printout for geocentric altitude above sea level in meters

--- NavJson.py
+++ NavJson.py
@@ -4,15 +4,26 @@ import ephem
 import datetime
 import json
 
-with open('NAV_SAT.json') as f:
-    data = json.load(f)
+# def_sat = 
+def_loc = ('42.37', '-71.03', '0')
 
-ctime = datetime.datetime.now()
-for sys in data.items():
-    for sat in sys[1]:
-        l1 = sat['id'].encode('utf-8')
-        l2 = sat['l2'].encode('utf-8')
-        l3 = sat['l3'].encode('utf-8')
-        sv = ephem.readtle(l1,l2,l3)
-        sv.compute(ctime)
-        print sv.ra, sv.dec
+def load_all_satellites():
+    with open('NAV_SAT.json') as f:
+        data = json.load(f)
+    return data
+
+def print_location_lle(data):
+
+    ctime = datetime.datetime.now()
+    for sys in data.items():
+        for sat in sys[1]:
+            l1 = sat['id'].encode('utf-8')
+            l2 = sat['l2'].encode('utf-8')
+            l3 = sat['l3'].encode('utf-8')
+            sv = ephem.readtle(l1,l2,l3)
+            sv.compute(ctime)
+            print sv.sublat, sv.sublong, sv.elevation
+
+data = load_all_satellites()
+print_location_lle(data)
+
